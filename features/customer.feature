1bcff5434f1755e854c4fec7cbdbe6354c50d272
Chris Bloom <chrisbloom7@gmail.com>
2011-12-22 23:37:11 -0500

    Adding 'Trainer cannot delete customers' scenario

--- features/customer.feature
+++ features/customer.feature
@@ -2,18 +2,35 @@
 Feature: Manage customers
   As a user of the system
   trainers
-  want to be able to create customers
+  want to be able to manage customers
  
-  Scenario: Create customer
+  Scenario: Trainer can create customers
     Given I am logged in as a trainer
     When I go to the Customers page
     Then I should be able to create a new customer
 
-  @wip
-  Scenario: Edit Customer
+  Scenario: Trainers can edit customers
     Given I am logged in as a trainer
     When the following customer exists:
         | first_name | last_name |
         | Birtha     | Jones     |
       And I go to the Customers page
     Then I should be able to edit the customer profile for "Birtha Jones"
+    
+  Scenario: Trainers cannot delete customers
+    Given I am logged in as a trainer
+    When the following customer exists:
+        | first_name | last_name | email             |
+        | Birtha     | Jones     | birtha@jones.name |
+      And I go to the Customers page
+    Then I should not be able to delete the customer profile for "Birtha Jones" with email "birtha@jones.name"
+
+  # Scenario: Admins can delete customers
+  #   Given I am logged in as a trainer
+  #   When the following customers exists:
+  #       | first_name | last_name | email             |
+  #       | Birtha     | Jones     | birtha@jones.name |
+  #       | Bobby      | Jones     | bobby@jones.name  |
+  #     And I go to the Customers page
+  #   Then I should be able to delete the customer profile for "Birtha Jones" with email "birtha@jones.name"
+  #     And I should still see the customer profile for "Bobby Jones"
--- features/step_definitions/customer_steps.rb
+++ features/step_definitions/customer_steps.rb
@@ -29,15 +29,30 @@ When /^I should be able to create a new customer$/ do
   fill_in 'customer[zip]', :with => '90210'
   select 'Multnomah', :from => 'customer[county]'
   click_button 'Save'
-  page.should have_content('Customer was successfully created.')
+  page.should have_text('Customer was successfully created.')
 end
 
 When /^I should be able to edit the customer profile for "(\w+) (\w+)"$/ do |first_name, last_name|
   click_link "#{last_name}, #{first_name}"
   fill_in 'customer[address]', :with => '123 Hopefully The Factory Isn\'t Using This Address Too Blvd'
   click_button 'Save'
-  page.should have_content('Customer was successfully updated.')
+  page.should have_text('Customer was successfully updated.')
   page.has_field?('customer[address]', :with => '123 Hopefully The Factory Isn\'t Using This Address Too Blvd')
+end
+
+# We need to be able to specify the email address so that we can 
+# lookup the customer record by a unique value in order to get the 
+# customer ID in order to find the correct form within the customer
+# list table. Complicated, I know, but better than hacking up the 
+# table HTML with superfluous identifiers just for this test.
+When /^I should not be able to delete the customer profile for "(\w+) (\w+)" with email "(.*)"$/ do |first_name, last_name, email|
+  customer = Customer.find_by_email(email)
+  find("a[href='/customers/#{customer.id}']").should have_content("#{last_name}, #{first_name}")
+  page.should have_no_selector("form[action='/customers/#{customer.id}'] input[type=submit][value=Delete]")
+end
+
+When /^I should be able to delete the customer profile for "(\w+) (\w+)" with email "(.*)"$/ do |first_name, last_name, email|
+  pending
 end
 
 Then /^show me the page$/ do
