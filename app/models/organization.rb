aaa078f867fe8f386152568706b6c478a76c5002
Mike Mueller <mike.mueller@panopticdev.com>
2012-01-10 00:56:50 -0800

    Implement organization management interfaces.

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,5 +1,14 @@
 class Organization < ActiveRecord::Base
   belongs_to :organization_type
+  belongs_to :parent, :class_name => "Organization"
 
   validates_presence_of :organization_type
+  validates_presence_of :parent, :if => :is_cmo?
+
+  has_many :children, :class_name => "Organization",
+           :foreign_key => "parent_id", :dependent => :nullify
+
+  def is_cmo?
+    organization_type.name == 'Case Management Organization'
+  end
 end
