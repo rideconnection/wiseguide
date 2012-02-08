4693275e72660cd43a7700994b624d902e445030
Chris Bloom <chrisbloom7@gmail.com>
2012-02-08 01:15:01 -0500

    Make sure sti classes are available in all environments where cacheing is disabled, and make sure we are loading those class files using a complete path based on Rails.root

--- config/initializers/preload_sti_models.rb
+++ config/initializers/preload_sti_models.rb
@@ -1,6 +1,6 @@
-if Rails.env.development?
+if !Rails.configuration.action_controller.perform_caching?
   # Pre-loaded our STI subclasses in development so Kase.descendants is properly populated
   %w[kase coaching_kase training_kase].each do |c|
-    require_dependency File.join("app","models","#{c}.rb")
+    require_dependency Rails.root.join("app","models","#{c}.rb").to_s
   end
 end
