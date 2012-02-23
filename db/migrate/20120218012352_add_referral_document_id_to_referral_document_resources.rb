cce8905ddd886d9385a016e96296f2334b2ef3af
Mike Mueller <mike.mueller@panopticdev.com>
2012-02-22 23:41:32 -0800

    Fix too-long database index name.
    Migration did not apply successfully for me with postgres due to 63-char
    limitation.  Shortened the index name to fix.

--- db/migrate/20120218012352_add_referral_document_id_to_referral_document_resources.rb
+++ db/migrate/20120218012352_add_referral_document_id_to_referral_document_resources.rb
@@ -3,11 +3,11 @@ class AddReferralDocumentIdToReferralDocumentResources < ActiveRecord::Migration
     add_column :referral_document_resources, :referral_document_id, :integer
     add_index :referral_document_resources, :referral_document_id
     remove_index :referral_document_resources, :resource_id
-    add_index :referral_document_resources, [:referral_document_id, :resource_id], :unique => true, :name => "idx_referral_document_resources_referral_document_id_resource_id"
+    add_index :referral_document_resources, [:referral_document_id, :resource_id], :unique => true, :name => "idx_refdoc_resources_refdoc_id_resource_id"
   end
 
   def self.down
-    remove_index :referral_document_resources, :column => [:referral_document_id, :resource_id], :name => "idx_referral_document_resources_referral_document_id_resource_id"
+    remove_index :referral_document_resources, :column => [:referral_document_id, :resource_id], :name => "idx_refdoc_resources_refdoc_id_resource_id"
     add_index :referral_document_resources, :resource_id
     remove_index :referral_document_resources, :referral_document_id
     remove_column :referral_document_resources, :referral_document_id
