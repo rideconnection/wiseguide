b5fd4a3e6ffd6f8300cff5b930a45e02b3d6e1e3
Mike Mueller <mike.mueller@panopticdev.com>
2012-02-23 01:10:19 -0800

    Show associated cases in dashboard.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,12 @@ class HomeController < ApplicationController
 
   def index
     if current_user.is_outside_user? then
+      org_assessments = current_user.organization.assessment_requests
+      @pending_assessments = org_assessments.where(:kase_id => nil)
+      @recent_cases = []
+      org_assessments.each do |request|
+        @recent_cases << request.kase unless request.kase.nil?
+      end
       render :outside_dashboard
     end
   end
