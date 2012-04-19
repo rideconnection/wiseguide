d28c8b48e7d33d1eda2c359471c09e269f561bdc
Kevin Chambers <KChambers@RideConnection.org>
2012-04-19 13:54:28 -0700

    Temporary change to allow blank case manager in coaching cases, turn off automatic emails to case managers

--- app/models/coaching_kase.rb
+++ app/models/coaching_kase.rb
@@ -1,7 +1,7 @@
 class CoachingKase < Kase
   belongs_to :case_manager, :foreign_key => :case_manager_id, :class_name=>'User'
   
-  validates :case_manager, :presence => true, :associated => true
+  # validates :case_manager, :presence => true, :associated => true
   validates :case_manager_notification_date, :allow_blank => true, :date => { 
     :before_or_equal_to => Proc.new { Date.current } 
   }
