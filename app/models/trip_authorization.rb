7033d48c7ea3d99a8e3ee7f6074a809345dc4e05
Chris Bloom <chrisbloom7@gmail.com>
2013-04-15 07:32:31 -0400

    Adding TripAuthorization#start_date to attr_accessible list

--- app/models/trip_authorization.rb
+++ app/models/trip_authorization.rb
@@ -6,7 +6,7 @@ class TripAuthorization < ActiveRecord::Base
   belongs_to :kase
   belongs_to :disposition_user, class_name: 'User'
   attr_accessible :allowed_trips_per_month, :disposition_date, :end_date,
-    :disposition_user_id, :kase_id
+    :disposition_user_id, :kase_id, :start_date
   validates :allowed_trips_per_month, numericality: { greater_than_or_equal_to: 1 }
   validates :start_date, date: true
   validates :end_date, allow_blank: true, date: { after_or_equal_to: :start_date }
