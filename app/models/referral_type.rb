796d3993b822b9ee4e7714a36912a21fd9deadb1
Mike Leone <michael.leone@panopticdev.com>
2013-06-27 18:27:33 -0400

    Validate names for new referral types. #1290.

--- app/models/referral_type.rb
+++ app/models/referral_type.rb
@@ -3,4 +3,7 @@ class ReferralType < ActiveRecord::Base
   belongs_to :created_by, :foreign_key => :created_by_id, :class_name=>'User'
   belongs_to :updated_by, :foreign_key => :updated_by_id, :class_name=>'User'
 
+  validates_format_of :name, 
+    :with => /\A(CC|TC).*\Z/, 
+    :message => 'must begin with either "CC" or "TC"'
 end
