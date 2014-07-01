0fe4e3433706e52af1c1f4b38936b2b32487a952
Chris Bloom <chrisbloom7@gmail.com>
2014-07-01 15:31:30 -0400

    Revert "Updated ruby string for capistrano"
    This reverts commit c3b539e8b0bc4b27d5a8348160ea305d4bb1d13e.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,5 +1,5 @@
 set :branch, "master"
-set :rvm_ruby_string, '1.9.3-p547@wiseguide'
+set :rvm_ruby_string, '1.9.2-p290@wiseguide'
 set :rails_env, "staging"
 role :web, "184.154.158.74"
 role :app, "184.154.158.74"
