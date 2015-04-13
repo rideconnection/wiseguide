5c9f10b947a768f1b3aa9fb1c491285ad61b8833
Chris Bloom <chrisbloom7@gmail.com>
2015-04-13 18:00:18 -0400

    upgrading after_build syntax in factories

--- spec/factories/referral_documents.rb
+++ spec/factories/referral_documents.rb
@@ -5,7 +5,7 @@ FactoryGirl.define do
     last_printed_at nil
 
     factory :referral_document do
-      after_build do |f|
+      after(:build) do |f|
         f.referral_document_resources << FactoryGirl.build(:referral_document_resource, :referral_document => nil)
       end
     end
