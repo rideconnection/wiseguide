9c8adf1c569bf00a39c1fe2047c5b1abdb007861
Chris Bloom <chrisbloom7@gmail.com>
2015-05-19 17:39:57 -0400

    Add SP to outcomes

--- app/controllers/outcomes_controller.rb
+++ app/controllers/outcomes_controller.rb
@@ -9,14 +9,13 @@ class OutcomesController < ApplicationController
   end
 
   def new
-    @outcome = Outcome.new(:kase_id=>params[:kase_id])
+    @outcome.kase_id = params[:kase_id]
     prep_edit
   end
 
   def create
-    @kase = Kase.find(params[:outcome][:kase_id])
+    @kase = Kase.find(@outcome.kase_id)
     authorize! :edit, @kase
-    @outcome = Outcome.new(params[:outcome])
 
     if @outcome.save
       redirect_to(@kase, :notice => 'Outcome was successfully created.') 
@@ -27,9 +26,10 @@ class OutcomesController < ApplicationController
   end
 
   def update
-    @kase = Kase.find(params[:outcome][:kase_id])
+    @kase = Kase.find(@outcome.kase_id)
     authorize! :edit, @kase
-    if @outcome.update_attributes(params[:outcome])
+
+    if @outcome.update_attributes(outcome_params)
       redirect_to(@kase, :notice => 'Outcome was successfully updated.') 
     else
       prep_edit
@@ -38,13 +38,31 @@ class OutcomesController < ApplicationController
   end
 
   def destroy
+    @kase = Kase.find(@outcome.kase_id)
+    authorize! :edit, @kase
+
     @outcome.destroy
     redirect_to(kase_path(@outcome.kase), :notice => 'Outcome was successfully deleted.')
   end
 
   private
+
   def prep_edit
     @trip_reasons = TripReason.accessible_by(current_ability)
   end
 
+  def outcome_params
+    params.require(:outcome).permit(
+      :exit_trip_count,
+      :exit_vehicle_miles_reduced,
+      :kase_id,
+      :six_month_trip_count,
+      :six_month_unreachable,
+      :six_month_vehicle_miles_reduced,
+      :three_month_trip_count,
+      :three_month_unreachable,
+      :three_month_vehicle_miles_reduced,
+      :trip_reason_id,
+    )
+  end
 end
