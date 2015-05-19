02ddc5338fb748e0d477a4c2b6ff517783d8c618
Chris Bloom <chrisbloom7@gmail.com>
2015-05-19 17:28:27 -0400

    Add SP to referral_documents

--- app/controllers/referral_documents_controller.rb
+++ app/controllers/referral_documents_controller.rb
@@ -6,7 +6,7 @@ class ReferralDocumentsController < ApplicationController
   def show; end
 
   def new
-    @referral_document = ReferralDocument.new(:kase_id => params[:kase_id])
+    @referral_document.kase_id = params[:kase_id]
     prep_edit
   end
 
@@ -18,8 +18,6 @@ class ReferralDocumentsController < ApplicationController
     @kase = Kase.find(params[:referral_document][:kase_id])
     authorize! :edit, @kase
     
-    @referral_document = ReferralDocument.new(params[:referral_document])
-
     if @referral_document.save
       redirect_to(@kase, :notice => 'Referral document was successfully created.')
     else
@@ -29,10 +27,10 @@ class ReferralDocumentsController < ApplicationController
   end
 
   def update
-    @kase = Kase.find(params[:referral_document][:kase_id])
+    @kase = Kase.find(@referral_document.kase_id)
     authorize! :edit, @kase
     
-    if @referral_document.update_attributes(params[:referral_document])
+    if @referral_document.update_attributes(referral_document_params)
       redirect_to(@kase, :notice => 'Referral document was successfully updated.')
     else
       prep_edit
@@ -41,20 +39,29 @@ class ReferralDocumentsController < ApplicationController
   end
 
   def destroy
+    @kase = Kase.find(@referral_document.kase_id)
+    authorize! :edit, @kase
+
     @referral_document.destroy
     redirect_to(kase_path(@referral_document.kase), :notice => 'Referral document was successfully deleted.')
   end
   
-private
+  private
   
   def prep_edit
     @resources = Resource.active
-    if !params[:referral_document].blank? and !params[:referral_document][:referral_document_resources_attributes].blank?
-      params[:referral_document][:referral_document_resources_attributes].each do |referral_document_resource|
-        @referral_document.referral_document_resources.build(ReferralDocumentResource.new.attributes.merge(referral_document_resource[1].slice(*ReferralDocumentResource.new.attributes.keys)))
-      end
-    elsif @referral_document.referral_document_resources.count == 0
+    if @referral_document.referral_document_resources.count == 0
       @referral_document.referral_document_resources.build
     end
+  end
+  
+  def referral_document_params
+    params.require(:referral_document).permit(
+      :kase_id,
+      :referral_document_resources_attributes => [
+        :note,
+        :resource_id
+      ],
+    )
   end
 end
--- app/models/referral_document_resource.rb
+++ app/models/referral_document_resource.rb
@@ -1,6 +1,6 @@
-class ReferralDocumentResource < ActiveRecord::Base
-  attr_protected :last_printed_at
-  
+class ReferralDocumentResource < ActiveRecord::Base  
+  include ActiveModel::ForbiddenAttributesProtection
+
   stampable :creator_attribute => :created_by_id, :updater_attribute => :updated_by_id
   belongs_to :created_by, :foreign_key => :created_by_id, :class_name=>'User'
   belongs_to :updated_by, :foreign_key => :updated_by_id, :class_name=>'User'
