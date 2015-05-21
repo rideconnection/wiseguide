7b3d62df3bcfac7eb6873138585eb4bcd795241d
Chris Bloom <chrisbloom7@gmail.com>
2015-05-21 09:17:14 -0400

    Adding SP to agencies

--- app/models/agency.rb
+++ app/models/agency.rb
@@ -1,5 +1,7 @@
 class Agency < ActiveRecord::Base
-  attr_accessible :name
+  include ActiveModel::ForbiddenAttributesProtection
+  # attr_accessible :name
+
   validates_presence_of :name
   default_scope order(:name)
 end
