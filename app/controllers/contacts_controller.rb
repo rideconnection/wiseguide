f51c326f232d04c7b63ebae17b3e6c1f722adda9
Chris Bloom <chrisbloom7@gmail.com>
2015-05-21 09:03:15 -0400

    Adding SP to contacts

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -19,18 +19,19 @@ class ContactsController < ApplicationController
   def new
     @readonly = false
     @contactable = params[:contact][:contactable_type].classify.constantize.find(params[:contact][:contactable_id])
-    @contact = Contact.new(params[:contact].merge({
+    @contact.attributes = {
       :date_time => DateTime.current,
       :user      => current_user
-    }))
+    }
     prep_edit
   end
 
   def create
     @contactable = params[:contact][:contactable_type].classify.constantize.find(params[:contact][:contactable_id])
     authorize! :edit, @contactable
-    @contact = Contact.new(params[:contact])
+
     @contact.user = current_user
+    
     if @contact.save
       redirect_to(@contactable, :notice => 'Contact was successfully created.') 
     else
@@ -42,7 +43,8 @@ class ContactsController < ApplicationController
   def update
     @contactable = @contact.contactable
     authorize! :edit, @contactable
-    if @contact.update_attributes(params[:contact])
+
+    if @contact.update_attributes(contact_params)
       redirect_to(@contact.contactable, :notice => 'Contact was successfully updated.') 
     else
       prep_edit
@@ -52,6 +54,7 @@ class ContactsController < ApplicationController
 
   def destroy
     authorize! :edit, @contact.contactable
+
     @contact.destroy
     redirect_to(@contact.contactable, :notice => 'Contact was successfully deleted.') 
   end
@@ -60,5 +63,17 @@ class ContactsController < ApplicationController
 
   def prep_edit
     @contact_methods = ['Phone', 'Email', 'Meeting', 'Case Action']
+  end
+  
+  def contact_params
+    params.require(:contact).permit(
+      :contactable_id,
+      :contactable_type,
+      :date_time,
+      :description,
+      :method,
+      :notes,
+      :show_full_notes,
+    )
   end
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,6 @@
 class Contact < ActiveRecord::Base
+  include ActiveModel::ForbiddenAttributesProtection
+
   belongs_to :user
   stampable :creator_attribute => :created_by_id, :updater_attribute => :updated_by_id
   belongs_to :created_by, :foreign_key => :created_by_id, :class_name=>'User'
