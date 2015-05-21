7c1ea8742d5604219f6ead1ff3d09502c742d3af
Chris Bloom <chrisbloom7@gmail.com>
2015-05-21 08:50:15 -0400

    Adding SP to customer support network members

--- app/controllers/customer_support_network_members_controller.rb
+++ app/controllers/customer_support_network_members_controller.rb
@@ -5,13 +5,12 @@ class CustomerSupportNetworkMembersController < ApplicationController
   end
 
   def new
-    @customer_support_network_member = CustomerSupportNetworkMember.new(:customer_id=>params[:customer_id])
+    @customer_support_network_member.attributes = {:customer_id=>params[:customer_id]}
   end
 
   def create
     @customer = Customer.find(params[:customer_support_network_member][:customer_id])
     authorize! :edit, @customer
-    @customer_support_network_member = CustomerSupportNetworkMember.new(params[:customer_support_network_member])
 
     if @customer_support_network_member.save
       redirect_to(@customer, :notice => 'Customer Support Network Member was successfully created.') 
@@ -23,7 +22,8 @@ class CustomerSupportNetworkMembersController < ApplicationController
   def update
     @customer = Customer.find(params[:customer_support_network_member][:customer_id])
     authorize! :edit, @customer
-    if @customer_support_network_member.update_attributes(params[:customer_support_network_member])
+
+    if @customer_support_network_member.update_attributes(customer_support_network_member_params)
       redirect_to(@customer, :notice => 'Customer Support Network Member was successfully updated.') 
     else
       render :action => "edit"
@@ -36,4 +36,16 @@ class CustomerSupportNetworkMembersController < ApplicationController
     redirect_to(@customer)
   end
 
+  private
+  
+  def customer_support_network_member_params
+    params.require(:customer_support_network_member).permit(
+      :customer_id,
+      :email,
+      :name,
+      :organization,
+      :phone_number,
+      :title,
+    )
+  end
 end
