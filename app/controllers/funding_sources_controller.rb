f7c454aed9d48c6acbc414665bdca9c7f67603ff
Chris Bloom <chrisbloom7@gmail.com>
2015-05-21 08:20:21 -0400

    Adding SP to funding sources

--- app/controllers/funding_sources_controller.rb
+++ app/controllers/funding_sources_controller.rb
@@ -1,10 +1,9 @@
 class FundingSourcesController < ApplicationController
   load_and_authorize_resource
+
   # GET /funding_sources
   # GET /funding_sources.xml
   def index
-    @funding_sources = FundingSource.all
-
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @funding_sources }
@@ -14,8 +13,6 @@ class FundingSourcesController < ApplicationController
   # GET /funding_sources/1
   # GET /funding_sources/1.xml
   def show
-    @funding_source = FundingSource.find(params[:id])
-
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @funding_source }
@@ -25,8 +22,6 @@ class FundingSourcesController < ApplicationController
   # GET /funding_sources/new
   # GET /funding_sources/new.xml
   def new
-    @funding_source = FundingSource.new
-
     respond_to do |format|
       format.html # new.html.erb
       format.xml  { render :xml => @funding_source }
@@ -35,14 +30,11 @@ class FundingSourcesController < ApplicationController
 
   # GET /funding_sources/1/edit
   def edit
-    @funding_source = FundingSource.find(params[:id])
   end
 
   # POST /funding_sources
   # POST /funding_sources.xml
   def create
-    @funding_source = FundingSource.new(params[:funding_source])
-
     respond_to do |format|
       if @funding_source.save
         format.html { redirect_to(@funding_source, :notice => 'Funding source was successfully created.') }
@@ -57,10 +49,8 @@ class FundingSourcesController < ApplicationController
   # PUT /funding_sources/1
   # PUT /funding_sources/1.xml
   def update
-    @funding_source = FundingSource.find(params[:id])
-
     respond_to do |format|
-      if @funding_source.update_attributes(params[:funding_source])
+      if @funding_source.update_attributes(funding_source_params)
         format.html { redirect_to(@funding_source, :notice => 'Funding source was successfully updated.') }
         format.xml  { head :ok }
       else
@@ -73,12 +63,17 @@ class FundingSourcesController < ApplicationController
   # DELETE /funding_sources/1
   # DELETE /funding_sources/1.xml
   def destroy
-    @funding_source = FundingSource.find(params[:id])
     @funding_source.destroy
 
     respond_to do |format|
       format.html { redirect_to(funding_sources_url) }
       format.xml  { head :ok }
     end
+  end
+  
+  private
+  
+  def funding_source_params
+    params.require(:funding_source).permit(:name)
   end
 end
