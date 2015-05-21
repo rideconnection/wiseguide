c2defdb5f696897117791cbe0f698701e0936eeb
Chris Bloom <chrisbloom7@gmail.com>
2015-05-21 09:19:23 -0400

    Adding SP to ada_service_eligibility_statuses

--- app/controllers/ada_service_eligibility_statuses_controller.rb
+++ app/controllers/ada_service_eligibility_statuses_controller.rb
@@ -4,8 +4,6 @@ class AdaServiceEligibilityStatusesController < ApplicationController
   # GET /ada_service_eligibility_statuses
   # GET /ada_service_eligibility_statuses.xml
   def index
-    @ada_service_eligibility_statuses = AdaServiceEligibilityStatus.all
-
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @ada_service_eligibility_statuses }
@@ -15,8 +13,6 @@ class AdaServiceEligibilityStatusesController < ApplicationController
   # GET /ada_service_eligibility_statuses/1
   # GET /ada_service_eligibility_statuses/1.xml
   def show
-    @ada_service_eligibility_status = AdaServiceEligibilityStatus.find(params[:id])
-
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @ada_service_eligibility_status }
@@ -26,9 +22,6 @@ class AdaServiceEligibilityStatusesController < ApplicationController
   # GET /ada_service_eligibility_statuses/new
   # GET /ada_service_eligibility_statuses/new.xml
   def new
-    authorize! :edit, AdaServiceEligibilityStatus
-    @ada_service_eligibility_status = AdaServiceEligibilityStatus.new
-
     respond_to do |format|
       format.html # new.html.erb
       format.xml  { render :xml => @ada_service_eligibility_status }
@@ -37,16 +30,11 @@ class AdaServiceEligibilityStatusesController < ApplicationController
 
   # GET /ada_service_eligibility_statuses/1/edit
   def edit
-    @ada_service_eligibility_status = AdaServiceEligibilityStatus.find(params[:id])
-    authorize! :edit, @ada_service_eligibility_status
   end
 
   # POST /ada_service_eligibility_statuses
   # POST /ada_service_eligibility_statuses.xml
   def create
-    authorize! :edit, AdaServiceEligibilityStatus
-    @ada_service_eligibility_status = AdaServiceEligibilityStatus.new(params[:ada_service_eligibility_status])
-
     respond_to do |format|
       if @ada_service_eligibility_status.save
         format.html { redirect_to(@ada_service_eligibility_status, :notice => 'Ada service eligibility status was successfully created.') }
@@ -61,11 +49,8 @@ class AdaServiceEligibilityStatusesController < ApplicationController
   # PUT /ada_service_eligibility_statuses/1
   # PUT /ada_service_eligibility_statuses/1.xml
   def update
-    @ada_service_eligibility_status = AdaServiceEligibilityStatus.find(params[:id])
-    authorize! :destroy, @ada_service_eligibility_status
-
     respond_to do |format|
-      if @ada_service_eligibility_status.update_attributes(params[:ada_service_eligibility_status])
+      if @ada_service_eligibility_status.update_attributes(ada_service_eligibility_status_params)
         format.html { redirect_to(@ada_service_eligibility_status, :notice => 'Ada service eligibility status was successfully updated.') }
         format.xml  { head :ok }
       else
@@ -78,13 +63,17 @@ class AdaServiceEligibilityStatusesController < ApplicationController
   # DELETE /ada_service_eligibility_statuses/1
   # DELETE /ada_service_eligibility_statuses/1.xml
   def destroy
-    @ada_service_eligibility_status = AdaServiceEligibilityStatus.find(params[:id])
-    authorize! :destroy, @ada_service_eligibility_status
     @ada_service_eligibility_status.destroy
 
     respond_to do |format|
       format.html { redirect_to(ada_service_eligibility_statuses_url) }
       format.xml  { head :ok }
     end
+  end
+  
+  private
+  
+  def ada_service_eligibility_status_params
+    params.require(:ada_service_eligibility_status).permit(:name)
   end
 end
