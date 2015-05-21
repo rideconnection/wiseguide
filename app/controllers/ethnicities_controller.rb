f6a667661452c2f43c548e5bfdb10fd562ec0a86
Chris Bloom <chrisbloom7@gmail.com>
2015-05-21 08:31:01 -0400

    Adding SP to ethnicities

--- app/controllers/ethnicities_controller.rb
+++ app/controllers/ethnicities_controller.rb
@@ -1,10 +1,9 @@
 class EthnicitiesController < ApplicationController
   load_and_authorize_resource
+
   # GET /ethnicities
   # GET /ethnicities.xml
   def index
-    @ethnicities = Ethnicity.all
-
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @ethnicities }
@@ -14,8 +13,6 @@ class EthnicitiesController < ApplicationController
   # GET /ethnicities/1
   # GET /ethnicities/1.xml
   def show
-    @ethnicity = Ethnicity.find(params[:id])
-
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @ethnicity }
@@ -25,8 +22,6 @@ class EthnicitiesController < ApplicationController
   # GET /ethnicities/new
   # GET /ethnicities/new.xml
   def new
-    @ethnicity = Ethnicity.new
-
     respond_to do |format|
       format.html # new.html.erb
       format.xml  { render :xml => @ethnicity }
@@ -35,14 +30,11 @@ class EthnicitiesController < ApplicationController
 
   # GET /ethnicities/1/edit
   def edit
-    @ethnicity = Ethnicity.find(params[:id])
   end
 
   # POST /ethnicities
   # POST /ethnicities.xml
   def create
-    @ethnicity = Ethnicity.new(params[:ethnicity])
-
     respond_to do |format|
       if @ethnicity.save
         format.html { redirect_to(@ethnicity, :notice => 'Ethnicity was successfully created.') }
@@ -57,10 +49,8 @@ class EthnicitiesController < ApplicationController
   # PUT /ethnicities/1
   # PUT /ethnicities/1.xml
   def update
-    @ethnicity = Ethnicity.find(params[:id])
-
     respond_to do |format|
-      if @ethnicity.update_attributes(params[:ethnicity])
+      if @ethnicity.update_attributes(ethnicity_params)
         format.html { redirect_to(@ethnicity, :notice => 'Ethnicity was successfully updated.') }
         format.xml  { head :ok }
       else
@@ -73,12 +63,17 @@ class EthnicitiesController < ApplicationController
   # DELETE /ethnicities/1
   # DELETE /ethnicities/1.xml
   def destroy
-    @ethnicity = Ethnicity.find(params[:id])
     @ethnicity.destroy
 
     respond_to do |format|
-      format.html { redirect_to(ethnicities_url) }
+      format.html { redirect_to(ethnicities_url, :notice => 'Ethnicity was successfully deleted.') }
       format.xml  { head :ok }
     end
+  end
+  
+  private
+  
+  def ethnicity_params
+    params.require(:ethnicity).permit(:name)
   end
 end
