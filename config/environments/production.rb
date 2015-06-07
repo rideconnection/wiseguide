f0b3ff969748f8539a7dbe0b8a6f4589e281cb92
Chris Bloom <chrisbloom7@gmail.com>
2015-06-07 16:13:08 -0400

    Fixing asset precompile list for production and staging

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Wiseguide::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( style.css print.css )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
