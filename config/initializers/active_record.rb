3f973157d0239a12373db2540727300488aad660
Chris Bloom <chrisbloom7@gmail.com>
2015-06-07 10:08:53 -0400

    `Model#model_name` now returns an object instead of a string

--- config/initializers/active_record.rb
+++ config/initializers/active_record.rb
@@ -3,7 +3,7 @@
 module ActiveRecord
   class Base
     def self.human_name
-      self.model_name.underscore.humanize.titlecase
+      self.model_name.to_s.underscore.humanize.titlecase
     end
 
     def self.humanized_name
