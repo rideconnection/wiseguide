d44900d3c186f88fe179ab4e13bc2d71672661a2
Chris Bloom <chrisbloom7@gmail.com>
2016-02-15 19:54:58 -0500

    Fixing asset precompile list after upgrade

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,4 +5,4 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( style.css print.css ) # *.png *.jpg *.jpeg *.gif
+Rails.application.config.assets.precompile += %w( style.css print.css *.png *.jpg *.jpeg *.gif )
